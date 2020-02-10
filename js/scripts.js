3159b8bb7173f28d88d611f22b83cc041fe96e17
Drake Wilcox <drakewilcox@gmail.com>
2020-02-10 13:05:46 -0800

    refactor code to remove unecessary arguments in constructor and change get price prototype to use dot notation to update properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 // BUSINESS LOGIC
-function PizzaOrder(sizeResponse, toppingsResponse, premiumResponse) {
-  this.standardPrice = 7
-  this.sizeResponse = sizeResponse,
-  this.toppingsResponse = toppingsResponse,
-  this.premiumResponse = premiumResponse;
+function PizzaOrder() {
 }
 PizzaOrder.prototype.update = function(sizeResponse, toppingsResponse, premiumResponse) {
   this.sizeResponse = sizeResponse,
@@ -11,23 +7,22 @@ PizzaOrder.prototype.update = function(sizeResponse, toppingsResponse, premiumRe
   this.premiumResponse = premiumResponse;
 }
 PizzaOrder.prototype.getPrice = function() {
-  var upCharge = 0;
-  if (this.sizeResponse === "sm") {
-    upCharge = 0;
-  } if (this.sizeResponse === "md") {
-    upCharge += 2;
-  } else if (this.sizeResponse === "lg") {
-    upCharge += 3;
-  }
   var toppingsAmount = parseInt(this.toppingsResponse.length) * .50; 
   var premiumAmount = parseInt(this.premiumResponse.length) * 1;
-  return this.standardPrice + toppingsAmount + premiumAmount + upCharge; 
+  this.standardPrice = 7 + toppingsAmount + premiumAmount;
+  if (this.sizeResponse === "sm") {
+    this.standardPrice += 0
+  } if (this.sizeResponse === "md") {
+    this.standardPrice += 2;
+  } else if (this.sizeResponse === "lg") {
+    this.standardPrice += 3;
+  }
+  return this.standardPrice;
   }
 function makeOrder() {
   var sizeResponse = $("#size").val();
   var toppingsResponse = [];
   var premiumResponse = [];
-  
   $("input:checkbox[name=toppings]:checked").each(function() {
     var toppings = $(this).val();
     toppingsResponse.push(toppings);
@@ -36,16 +31,13 @@ function makeOrder() {
     var premium = $(this).val();
     premiumResponse.push(premium);
   });
-  
   pizzaOrder.update(sizeResponse, toppingsResponse, premiumResponse)
-  
-  var pizzaPrice = pizzaOrder.getPrice();
-  $("#output").text("$" + pizzaPrice);
+  pizzaOrder.getPrice();
+  $("#output").text("$" + pizzaOrder.standardPrice);
   $(".output").show();
 }
 // USER LOGIC 
 var pizzaOrder = new PizzaOrder();
-
 $(document).ready(function(){
   $("form.orderForm").submit(function(event){
     event.preventDefault();
